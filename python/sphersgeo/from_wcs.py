2b099908048d0210be93f2177d6d5580becaba42
zacharyburnett <zburnett@stsci.edu>
2025-07-25 15:49:04 -0400

    update `from_wcs` to match new logic from romancal

--- python/sphersgeo/from_wcs.py
+++ python/sphersgeo/from_wcs.py
@@ -10,17 +10,25 @@ except (ImportError, ModuleNotFoundError):
     warnings.warn("using `from_wcs` requires `gwcs` to be installed")
 
 
+__all__ = ["polygon_from_wcs"]
+
+
 def polygon_from_wcs(
     wcs: WCS | PathLike, extra_vertices_per_edge: int = 0
 ) -> SphericalPolygon:
-    r"""
-    Create a new ``SphericalPolygon`` from the footprint of a WCS specification.
+    """
+    Create a new `SphericalPolygon` from the footprint of a WCS specification.
 
-    This method requires ``gwcs`` be installed.
+    Parameters
+    ----------
+    wcs: WCS :
+        WCS object
+    extra_vertices_per_edge: int :
+        extra vertices to create on each edge to capture distortion (Default value = 0)
 
-    :param wcs: GWCS WCS object
-    :param extra_vertices_per_edge: extra vertices to create on each edge to capture distortion
-    :returns: polygon representing the footprint of the provided WCS
+    Returns
+    -------
+    polygon representing the footprint of the provided WCS
     """
 
     if not hasattr(wcs, "bounding_box") or wcs.bounding_box is None:
@@ -28,69 +36,44 @@ def polygon_from_wcs(
             "Cannot infer image footprint from WCS without a bounding box."
         )
 
-    image_shape = wcs.array_shape
+    array_shape = (
+        wcs.array_shape
+        if hasattr(wcs, "array_shape") and wcs.array_shape is not None
+        else tuple(
+            wcs.bounding_box[index][1] - wcs.bounding_box[index][0]
+            for index in range(len(wcs.bounding_box))
+        )
+    )
     if extra_vertices_per_edge <= 0:
         vertex_points = wcs.footprint(center=False)
     else:
         # constrain number of vertices to the maximum number of pixels on an edge, excluding the corners
-        if extra_vertices_per_edge > max(image_shape) - 2:
-            extra_vertices_per_edge = max(image_shape) - 2
+        if extra_vertices_per_edge > max(array_shape) - 2:
+            extra_vertices_per_edge = max(array_shape) - 2
 
         # build a list of pixel indices that represent equally-spaced edge vertices
-        edge_xs = np.linspace(
-            0,
-            image_shape[0],
-            num=extra_vertices_per_edge + 1,
-            endpoint=False,
+        vertices_per_edge = 2 + extra_vertices_per_edge
+        origin_indices = np.zeros(vertices_per_edge - 1) - 0.5
+        x_end_indices = array_shape[0] - origin_indices
+        y_end_indices = array_shape[1] - origin_indices
+        vertices_x = np.linspace(
+            0, array_shape[0], num=vertices_per_edge - 1, endpoint=False
         )
-        edge_ys = np.linspace(
-            0,
-            image_shape[1],
-            num=extra_vertices_per_edge + 1,
-            endpoint=False,
+        vertices_y = np.linspace(
+            0, array_shape[1], num=vertices_per_edge - 1, endpoint=False
         )
-        edge_indices = np.round(
-            np.concatenate(
-                [
-                    # north edge
-                    np.stack(
-                        [
-                            np.repeat(0, repeats=extra_vertices_per_edge + 1),
-                            edge_ys,
-                        ],
-                        axis=1,
-                    ),
-                    # east edge
-                    np.stack(
-                        [
-                            edge_xs,
-                            np.repeat(
-                                image_shape[1] - 1, repeats=extra_vertices_per_edge + 1
-                            ),
-                        ],
-                        axis=1,
-                    ),
-                    # south edge
-                    np.stack(
-                        [
-                            np.repeat(
-                                image_shape[0] - 1, repeats=extra_vertices_per_edge + 1
-                            ),
-                            edge_ys[::-1],
-                        ],
-                        axis=1,
-                    ),
-                    # west edge
-                    np.stack(
-                        [
-                            edge_xs[::-1],
-                            np.repeat(0, repeats=extra_vertices_per_edge + 1),
-                        ],
-                        axis=1,
-                    ),
-                ],
-                axis=0,
-            )
+        edge_indices = np.concatenate(
+            [
+                # north edge
+                np.stack([origin_indices, vertices_y], axis=1),
+                # east edge
+                np.stack([vertices_x, y_end_indices], axis=1),
+                # south edge
+                np.stack([x_end_indices, y_end_indices - vertices_y], axis=1),
+                # west edge
+                np.stack([x_end_indices - vertices_x, origin_indices], axis=1),
+            ],
+            axis=0,
         )
 
         # query the WCS for pixel indices at the edges
@@ -99,14 +82,9 @@ def polygon_from_wcs(
             axis=1,
         )
 
-    if hasattr(wcs, "array_shape") and wcs.array_shape is not None:
-        interior_point = SphericalPoint.from_lonlat(
-            wcs(wcs.array_shape[0] / 2.0, wcs.array_shape[1] / 2.0)
-        )
-    else:
-        interior_point = None
-
     return SphericalPolygon(
         MultiSphericalPoint.from_lonlat(vertex_points),
-        interior_point=interior_point,
+        interior_point=SphericalPoint.from_lonlat(
+            wcs(wcs.array_shape[0] / 2.0, wcs.array_shape[1] / 2.0)
+        ),
     )
