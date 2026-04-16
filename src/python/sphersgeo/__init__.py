4c5c8055c94765a809b7ae378428c7ac1886440c
zacharyburnett <zburnett@stsci.edu>
2026-04-16 13:27:45 -0400

    explicitly define members

--- src/python/sphersgeo/__init__.py
+++ src/python/sphersgeo/__init__.py
@@ -1,4 +1,22 @@
-from .sphersgeo import *
+import sphersgeo
+
+from .sphersgeo import (
+    ArcString,
+    MultiArcString,
+    MultiSphericalPoint,
+    MultiSphericalPolygon,
+    SphericalPoint,
+    SphericalPolygon,
+)
+
+__all__ = [
+    "SphericalPoint",
+    "MultiSphericalPoint",
+    "ArcString",
+    "MultiArcString",
+    "SphericalPolygon",
+    "MultiSphericalPolygon",
+]
 
 __doc__ = sphersgeo.__doc__
 if hasattr(sphersgeo, "__all__"):
