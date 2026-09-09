3d9ae2776e74d07f4e57ea59015028c7cea7e471
zacharyburnett <zburnett@stsci.edu>
2026-09-09 13:26:16 -0400

    no sidebar customization for furo

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,6 @@ html_theme_options = {
     "dark_logo": "https://github.com/spacetelescope/stsci-package-template/blob/0c4b13779e02ff9b8fb3585615e26d51cadcc14b/docs/_static/stsci_pri_combo_mark_dark_bkgd.png",
 }
 html_last_updated_fmt = "%b %d, %Y"
-html_sidebars = {"**": ["globaltoc.html", "relations.html", "searchbox.html"]}
 html_domain_indices = True
 html_use_index = True
 
